bc0dd7713b6737fc391070d90483fcc51181a1d2
MM <maximshuntov@gmail.com>
2025-09-23 23:35:01 +0300

    Styles

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import "../styles/Login.css"
+import { useState } from "react";
+
 function Login() {
+    const [name, setname] = useState("Name");
+    const [password, setpassword] = useState("");
     return (
         <div className="Login">
-            
             <div className="Auth">
-                <form>
+                <h1>Авторизация</h1>
+                <form action="inName" className="inName">
                     <label>
                         Name:
-                        <input type="text" name="name">
+                        <input type="text" name="name" placeholder="name">
                         </input>
                     </label>
                 </form>
-                <h1>Авторизация</h1>
-                <p1>Логин</p1><br/>
+                 <form action="inPass" className="inPass">
+                    <label>
+                        Pass:  
+                        <input type="password" name="password" placeholder="password">
+                        </input>
+                    </label>
+                </form>
+                <form>
+                    <button name="GO">Go</button>
+                </form>
+                {/* <p1>Логин</p1><br/>
                 <form type="text" ></form><br/>
                 <p2>Пароль</p2><br/>
-                <input type="password"></input><br/>
+                <input type="password"></input><br/> */}
             </div>
         </div>
     );
