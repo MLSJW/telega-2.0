b1602c1efb109bb0abc3322eed61124d9e353912
MM <maximshuntov@gmail.com>
2025-10-06 11:57:47 +0300

    cors

--- server/server.js
+++ server/server.js
@@ -5,16 +5,29 @@ const app = express();
 const userRouter = require('./routes/userRoutes')
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
+const cors = require('cors');
 
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send( { express: 'EXPRESS CONNECTED TO REACT' })
-})
+
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST']
+}
+
+app.use(cors(corsOptions))
+app.use(express.json({extended: true}))
+
+app.get('/api', (req, res) => {
+    res.json({ab:['fg','sdf']})
+});
+
 
 app.use('/api', userRouter)
+app.get('/api', (req, res) => {
+    res.sendFile(__dirname + 'package.json')
+});
 
 http.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
